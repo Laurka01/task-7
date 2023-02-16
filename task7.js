2f9ae7ff2ed08d87f3327703cce92c7eb222cb4c
Laura Dauletova <Laura.Dauletova@kcell.kz>
2023-02-16 17:28:30 +0600

    my m

--- task7.js
+++ task7.js
@@ -3,31 +3,44 @@ const postTemplate = document.getElementById('single-post');
 const fetchPostsButton = document.querySelector('#fetch')
 let API_URL = 'https://jsonplaceholder.typicode.com/posts';
 let API_URL_2 = 'https://jsonplaceholder.typicode.com/comments?postId=';
-function sendHttpRequest(method,url) {
-    return new Promise (resolve => {
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = 'json';
-        xhr.open(method, url);
-        xhr.onload=()=> {
-            resolve(xhr.response);
-        }
-        xhr.send();
-    })
-}
-sendHttpRequest('GET', API_URL).then(posts => {
+async function doGetRequest(url) {
+
+    let res = await axios.get(url);
+  
+    let data = res.data;
+    return data
+  }
+// sendHttpRequest('GET', API_URL).then(posts => {
+//     for (const post of posts) {
+//         const postEl = document.importNode(postTemplate.content, true);
+//         postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//         postEl.querySelector('p').textContent = post.body; 
+        
+//         sendHttpRequest('GET', `${API_URL_2}${post.id}`).then(comments => {
+//             let mylist = "";
+//             //console.log(comments)
+//             for (const comment of comments){
+//                 mylist += comment.body;
+//                 //const postEl = document.importNode(postTemplate.content, true);
+//             }  
+//             postEl.querySelector('h3').textContent = mylist;
+
+//         });              
+//         listElement.append(postEl);
+//     }
+// });
+
+doGetRequest(API_URL).then(posts => {
     for (const post of posts) {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body; 
-        let mylist;
-        sendHttpRequest('GET', `${API_URL_2}${post.id}`).then(comments => {
-            //console.log(comments)
+        let mylist = '';
+        doGetRequest(`${API_URL_2}${post.id}`).then(comments => {
             for (const comment of comments){
                 mylist += comment.body;
-                //const postEl = document.importNode(postTemplate.content, true);
             }  
             postEl.querySelector('h3').textContent = mylist;                
-
         });              
         listElement.append(postEl);
     }
